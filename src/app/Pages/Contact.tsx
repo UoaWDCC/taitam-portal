911929eef61b6358876e6ba94df7738b55cca1d4
AShenoy16 <ashe194@aucklanduni.ac.nz>
2023-05-12 15:42:04 +1200

    Added image to right of the page, added fonts

--- src/app/Pages/Contact.tsx
+++ src/app/Pages/Contact.tsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+"use client"
+
+import React from "react";
 import "./Contact.css"; // import the CSS file
-import PoppinsRegular from "../../Fonts/Poppins-Regular.ttf";
+import { Poppins } from "next/font/google";
+import contactSVG from "../Images/Illustration.svg";
+import Image from "next/image";
+
+
 
 type Props = {};
 
@@ -15,36 +20,41 @@ type Inputs = {
 
 export default function Contact({}: Props) {
   return (
-    <div className="contact">
-      <div className="contact-info">
-        <h1 className="contact-header">Contact us</h1>
-        <h2 className="contact-header-2">Have some questions</h2>
-        <p className="contact-interest">
-          Thank you for your interest. Please fill out the form and we will get
-          back to you promptly regarding your request
-        </p>
-      </div>
+    <div className="main">
+      <div className="contact">
+        <div className="contact-info">
+          <h1 className="contact-header">Contact us</h1>
+          <h2 className="contact-header-2">Have some questions</h2>
+          <p className="contact-interest">
+            Thank you for your interest. Please fill out the form and we will
+            get back to you promptly regarding your request
+          </p>
+        </div>
 
-      <div>
-        <form>
-          <div className="form">
-            <p className="form-info">Full Name*</p>
-            <input
-              className="input"
-              placeholder="Full Name"
-              name="Full Name"
-              type="text"
-            />
-            <p className="form-info">Email</p>
-            <input className="input" placeholder="Email" type="email" />
-            <p className="form-info">Message</p>
-            <textarea className="input" placeholder="Message" rows={5} />
+        <div>
+          <form>
+            <div className="form">
+              <p className="form-info">Full Name*</p>
+              <input
+                className="input"
+                placeholder="Full Name"
+                name="Full Name"
+                type="text"
+              />
+              <p className="form-info">Email</p>
+              <input className="input" placeholder="Email" type="email" />
+              <p className="form-info">Message</p>
+              <textarea className="input" placeholder="Message" rows={5} />
 
-            <div>
-              <button className="button">Submit</button>
+              <div>
+                <button className="button">Submit</button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
+      </div>
+      <div className="image">
+        <Image src={contactSVG} alt="contact" width={400} height = {400}/>
       </div>
     </div>
   );
