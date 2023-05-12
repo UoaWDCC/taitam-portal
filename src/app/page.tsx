972f4bacb69abdd850504a395797fada8fd84cbc
jrutor <jrut644@aucklanduni.ac.nz>
2023-05-12 15:27:31 +1200

    extract card to separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import {css} from "@linaria/core";
 import {Poppins} from 'next/font/google';
-
-import Image from 'next/image';
-import stockImg1 from "./events1.png";
-import heart from "./heart.png";
-
-const body = css`
-  font-family: sans-serif;
-  background-color: #FFF;
-  padding: 100px 160px;
-`;
+import {Card} from "@/app/components/bigCard";
+import stockImg1 from "./images/events1.png";
+import stockImg2 from "./images/events2.png";
+import stockImg3 from "./images/events3.png";
 
 const poppinsBlack = Poppins({weight: "900", subsets: ['latin']});
-const poppinsMedium = Poppins({weight: "500", subsets: ['latin']});
-const poppinsRegular = Poppins({weight: "400", subsets: ['latin']});
 const poppinsLight = Poppins({weight: "300", subsets: ['latin']});
 
 
@@ -28,61 +19,18 @@ export default function Home() {
                 with experienced tech professionals, employers, or recruiters, or simply want to meet other students
                 facing similar challenges, we have something for everyone.
             </div>
-            <div style={{
-                border: "solid black 1px",
-                boxShadow: "0 3px 0 4px",
-                display: "grid",
-                gridTemplateColumns: "53% 47%",
-                gridTemplateRows: "auto",
-                backgroundColor: "white",
-                margin: "50px 0"
-            }}>
-                <div style={{
-                    margin: "auto 0",
-                    gridColumnStart: "1",
-                    width: "100%",
-                    position: "relative",
-                    paddingTop: "50.7%"
-                }}>
-                    <Image
-                        src={stockImg1}
-                        alt="Stockholder image 1"
-                        fill={true}/>
-                </div>
-                <div style={{gridColumnStart: "2", margin: "20px 25px", position: "relative"}}>
-                    <p className={poppinsMedium.className}
-                       style={{margin: "0", fontSize: "16pt"}}>
-                        Launch Your Tech Career at Our Career Fair
-                    </p>
-                    <p className={poppinsRegular.className}
-                       style={{margin: "5px 0", fontSize: "12pt", color: "#056DFA"}}>
-                        30 APR 6pm @ OGGB Building, UOA
-                    </p>
-                    <p style={{lineHeight: "30px"}}>
-                        Connect with top employers, recruiters, and other talented students at our annual Tech Career
-                        Fair. Explore job opportunities, receive professional CV advice, and network with industry
-                        leaders.
-                    </p>
-                    <div style={{position: "absolute", bottom: "0", width: "100%"}}>
-                        <div className={poppinsMedium.className}
-                             style={{
-                                 float: "left",
-                                 backgroundColor: "#FFCC01",
-                                 boxShadow: "0 2px 0 1px",
-                                 width: "137px",
-                                 height: "40px",
-                                 borderRadius: "20px",
-                                 fontSize: "19px",
-                                 textAlign: "center",
-                             }}><p style={{margin: "8px 0"}}>Sign Up</p>
-                        </div>
-                        <div className={poppinsMedium.className} style={{float: "right", margin: "4px 0"}}>
-                            <Image src={heart} alt="heart"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <Card title={"Launch Your Tech Career at Our Career Fair"} 
+                  date={"30 APR 6pm @ OGGB Building, UOA"}
+                  paragraph={"Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."}
+                  image={stockImg1}/>
+            <Card title={"Launch Your Tech Career at Our Career Fair"} 
+                  date={"30 APR 6pm @ OGGB Building, UOA"}
+                  paragraph={"Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."}
+                  image={stockImg2}/>
+            <Card title={"Launch Your Tech Career at Our Career Fair"} 
+                  date={"30 APR 6pm @ OGGB Building, UOA"}
+                  paragraph={"Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."}
+                  image={stockImg3}/>
         </div>
     );
 }
