cf2cc1545f75d21663bb788d34ca7117367ffae3
amy <yyeo523@aucklanduni.ac.nz>
2023-05-17 18:43:09 +1200

    Navigation bar #1

---
+++ src/app/components/script.js
@@ -0,0 +1,19 @@
+// navLinkEls = document.querySelector('.linksContainer').querySelectorAll('a');
+// console.log(navLinkEls);
+
+// navLinkEls.forEach(element => {
+//     element.addEventListener('click', () => {
+//         navLinkEls.forEach(nav => nav.classList.remove('.active'))
+
+//         this.classList.add('.active');
+//     })
+// });    
+
+const navLinkEls = document.querySelectorAll('.navLinks');
+
+navLinkEls.forEach((navLinkEl) => {
+    navLinkEl.addEventListener('click', () => {
+        document.querySelector('.active')?.classList.remove('.active');
+        navLinkEl.classList.add('.active');
+    });
+});
