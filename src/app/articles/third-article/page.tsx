2295ab1ef03eac24c300fe41db37d0b0ddccdfb9
Wesley <wmer038@aucklanduni.ac.nz>
2023-08-25 15:15:58 +1200

    Article sub pages made responsive in tablet mode

--- src/app/articles/third-article/page.tsx
+++ src/app/articles/third-article/page.tsx
@@ -36,7 +36,20 @@ const headerImage = css`
     }
   }
 
-  @media only screen and (min-width: 601px) and (max-width: 1024px) {
+  @media only screen and (max-width: 1024px) {
+    margin-left: auto;
+    margin-right: auto;
+    z-index: -1;
+    position: absolute;
+    height: auto;
+
+    img {
+      max-width: 100%;
+      height: auto;
+      margin-left: 28px;
+      margin-top: 40px;
+      margin-right: 28px;
+    }
   }
 `;
 
@@ -53,6 +66,15 @@ const title = css`
     text-align: left;
     margin-left: 16px;
     margin-top: 55px;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 48px;
+    padding: 0;
+    color: #f96e47;
+    text-align: left;
+    margin-left: 34px;
+    margin-top: 43px;
   }
 `;
 
@@ -79,6 +101,14 @@ const cardContainer = css`
     flex-flow: column wrap;
     justify-content: space-between;
     margin-top: 90px;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    margin-left: 67px;
+    display: block;
+    flex-flow: column wrap;
+    justify-content: space-between;
+    margin-top: 230px;
   }
 `;
 
