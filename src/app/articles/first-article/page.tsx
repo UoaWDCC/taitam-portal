f81b9e01bf68fdb8e60a7ce9d83343af9865e5a4
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2023-09-09 15:39:29 +1200

    moved page.tsx back to original place as per new next.js routing documentation

--- src/app/articles/first-article/page.tsx
+++ src/app/articles/first-article/page.tsx
@@ -1,13 +1,13 @@
-// pages/[id].tsx
-import { useRouter } from "next/router";
-import { EventCard } from "@/app/(components)/bigCard"; // Import your EventCard component
-import { fetchArticlesFromNotion } from "../../artsData"; // Import your fetchArticlesFromNotion function
-import ArticlesCard from "@/app/(components)/ArticlesCard";
+import React from "react";
 import { css } from "@linaria/core";
-import Image from "next/image";   
+import Image from "next/image";
+import image1 from "../../(images)/articles1.png";
+import "../../globals.scss";
+import ArticlesCard from "../../(components)/ArticlesCard";
+import stockImg from "../../(images)/events1.png";
 
 const headerImage = css`
-  max-width: 1190px;
+  max-width: 1119px;
   height: 310px;
   display: block;
   margin-left: auto;
@@ -37,35 +37,24 @@ const cardContainer = css`
   margin-top: 280px;
 `;
 
-interface DynamicPageProps {
-  articles: artStruct; // Use the artStruct type here
-}
-
-const DynamicPage = ({ articles }: DynamicPageProps) => {
-  const router = useRouter();
-
-  // Find the article index that matches the articleId
-  const articleId = parseInt(router.query.articleId as string); // Convert to a number
-
-  const selectedArticle = articles[articleId];
-
+export default function FirstArticle() {
   return (
     <>
       <div className={root}>
         <h1 className={title}>ARTICLES</h1>
 
-        <div className={headerImage} >
-          <Image src={selectedArticle.cover}  width = {1190} height = {310} alt="Landing Image"></Image>
+        <div className={headerImage}>
+          <Image src={image1} alt="Landing Image"></Image>
         </div>
         <div className={cardContainer}>
           <div>
             <ArticlesCard
-              title={selectedArticle.name}
-              reference={`By ${selectedArticle.author} | ${selectedArticle.date.start}`}
-              par1={selectedArticle.desc}
+              title="Launch Your Tech Career at Our Career Fair"
+              reference="By Naren Rohan, Project Manager | 12 July 2023"
+              par1="In today's fast-paced digital age, technology has become an integral part of our daily lives. From innovative startups to multinational corporations, the demand for skilled tech professionals continues to grow. Our upcoming career fair offers an unparalleled opportunity to connect with leading companies, explore job prospects, and make meaningful connections that can shape your future."
               subtitle="A Networking Haven for Aspiring Tech Professionals:"
               par2="Attending a career fair can be a game-changer for aspiring tech professionals. It serves as a networking haven where you can meet and interact with representatives from top-tier companies actively seeking talented individuals like you. This face-to-face interaction provides a unique platform to showcase your skills, demonstrate your passion for technology, and make a lasting impression on potential employers."
-              image={selectedArticle.cover}
+              image={stockImg}
               par3="Our career fair is designed to cater to a wide range of tech career paths. Whether you are interested in software development, data analysis, cybersecurity, artificial intelligence, or any other tech field, there will be opportunities for you to explore. Companies from various sectors such as finance, healthcare, e-commerce, and entertainment will be in attendance, offering diverse avenues for career growth."
               par4="Aside from networking opportunities, our career fair also features informative sessions, workshops, and panel discussions conducted by industry experts. These sessions provide invaluable insights into the latest trends, technologies, and challenges in the tech industry. Whether it's a panel discussion on the future of AI or a workshop on mastering coding languages, you will gain knowledge that can give you a competitive edge in your job search and career advancement."
               par5="Attending a career fair is an excellent way to jumpstart your tech career. From networking with industry leaders to gaining valuable insights, our upcoming career fair promises to be an event that can shape your professional trajectory. Don't miss this opportunity to connect with top companies, explore exciting job prospects, and take your first step towards launching a successful tech career. Join us at our career fair and unlock a world of possibilities! Attending a career fair is an excellent way to jumpstart your tech career. From networking with industry leaders to gaining valuable insights, our upcoming career fair promises to be an event that can shape your professional trajectory. Don't miss this opportunity to connect with top companies, explore exciting job prospects, and take your first step towards launching a successful tech career. Join us at our career fair and unlock a world of possibilities! Attending a career fair is an excellent way to jumpstart your tech career. From networking with industry leaders to gaining valuable insights, our upcoming career fair promises to be an event that can shape your professional trajectory. Don't miss this opportunity to connect with top companies, explore exciting job prospects, and take your first step towards launching a successful tech career. Join us at our career fair and unlock a world of possibilities!"
@@ -75,12 +64,4 @@ const DynamicPage = ({ articles }: DynamicPageProps) => {
       </div>
     </>
   );
-};
-
-export default DynamicPage;
-
-// Fetch articles and pass them as props
-export async function getServerSideProps() {
-  const articles = await fetchArticlesFromNotion();
-  return { props: { articles } };
 }
---
+++ src/app/page.tsx
@@ -0,0 +1,362 @@
+﻿import { css } from "@linaria/core";
+import { Poppins } from "next/font/google";
+import React from "react";
+import Image from "next/image";
+import Card from "./(components)/Card";
+import Button from "./(components)/Button";
+import LandingImage from "./(images)/Landingimage.svg";
+import Mouse from "./(images)/mouse.svg";
+import Arrow from "./(images)/arrow.png";
+import BigImage from "./(images)/bigimage.png";
+import { fetchEventsFromNotion } from "../../eventsData";
+import { fetchArticlesFromNotion } from "../../artsData";
+
+const poppinsMedium = Poppins({ weight: "500", subsets: ["latin"] });
+
+const layoutContainer = css`
+  max-width: 1200px;
+  width: 100%;
+  padding: 0 50px;
+  box-sizing: border-box;
+  margin: -50px auto 100px auto;
+`;
+
+const title = css`
+  font-size: 96px;
+  padding: 0;
+  margin: 10px 0 0 0;
+  color: #f96e47;
+  text-align: left;
+`;
+
+const container = css`
+  margin: -100px 0 0 0;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-between;
+`;
+
+const cardContainer = css`
+  margin: 250px 0 0 0;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-between;
+`;
+
+const h3style = css`
+  font-size: 19px;
+  padding: 0;
+  margin: 0 0 0 0;
+  color: #000000;
+  text-align: left;
+`;
+
+const icon = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const cardrow = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: nowrap;
+`;
+
+const cardSpace = css`
+  margin: 0 35px 0 35px;
+`;
+
+const headerImage = css`
+  width: 100%;
+  margin-top: -80px;
+`;
+
+const MouseImage = css`
+  z-index: -1;
+  position: absolute;
+  left: 52%;
+`;
+
+function trimDescription(description: string, maxLength: number): string {
+  if (description.length > maxLength) {
+    return description.slice(0, maxLength) + "...";
+  }
+  return description;
+}
+
+
+// const fetchFromNotion = async () => {
+//   const res = await fetch(`${process.env.API_ENDPOINT}/notion`, {
+//     method: "GET",
+//     headers: {
+//       "Cache-Control": "no-store, no-cache",
+//     },
+//   });
+//   const data = await res.json();
+//   return JSON.parse(JSON.stringify(data));
+// };
+
+// const fetchArticlesFromNotion = async () => {
+
+//   const res = await fetch(`${process.env.API_ENDPOINT}/articlesApi`, {
+//     method: "GET",
+//     headers: {
+//       "Cache-Control": "no-store, no-cache",
+//     },
+    
+//   });
+//   const data = await res.json();
+//   return JSON.parse(JSON.stringify(data));
+// };
+
+
+// interface HomePageProps {
+//   events: rowsStruct; // Use the artStruct type here
+//   articles: artStruct;
+// }
+
+
+
+export default async function Home(){
+
+  const events:rowsStruct = await fetchEventsFromNotion();
+  const articles:artStruct = await fetchArticlesFromNotion();
+
+  return (
+        <>
+          {/* <Image className={headerImage} src={LandingImage} alt="Landing Image" /> */}
+    
+          <div className={layoutContainer}>
+            <div className={container}>
+              <h1 className={title}>WELCOME</h1>
+              <p style={{ marginRight: "200px" }}>
+                Taitamariki Potentia is a student-led tech community created to
+                bridge the gap between university and employment. Our mission is to
+                empower young people studying technical disciplines to navigate the
+                challenges associated with breaking into the industry.
+              </p>
+    
+              <div className={icon}>
+                <h3 className={h3style}>OUR HISTORY</h3>
+                <Image
+                  src={Arrow}
+                  alt="Arrow"
+                  className={icon}
+                  width={70}
+                  height={30}
+                ></Image>
+              </div>
+    
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Image src={BigImage} alt="Town" width={930} height={750} />
+              </div>
+            </div>
+    
+            <h3 className={h3style}>Upcoming</h3>
+            <h1 className={title}>EVENTS</h1>
+    
+            <div className={cardrow}>
+              {events.slice(0, 3).map((event, index) => (
+                <div key={index} className={index === 1 ? cardSpace : undefined}>
+                  <Card
+                    title={event.name}
+                    body={trimDescription(event.desc, 250)} //trim to 130 characters
+                    imageUrl={event.cover}
+                    btn={{
+                      text: "Sign Up",
+                      href: event.link,
+                      type: "primary",
+                      width: "cardButton",
+                      target: "_blank",
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+    
+            <div
+              className={poppinsMedium.className}
+              style={{
+                marginTop: "20px",
+              }}
+            >
+              <Button
+                text="Learn More"
+                href="/events"
+                type="primary"
+                width="largeButton"
+              ></Button>
+            </div>
+    
+            <div className={MouseImage}>
+              <Image src={Mouse} alt="Mouse Image" width={700}></Image>
+            </div>
+    
+            <div className={cardContainer}>
+              <h3 className={h3style}>Latest</h3>
+              <h1 className={title}>ARTICLES</h1>
+    
+              <div className={cardrow}>
+                {articles.slice(0, 3).map((art, index) => (
+                  <div key={index} className={index === 1 ? cardSpace : undefined}>
+                    <Card
+                      title={art.name}
+                      body={trimDescription(art.desc, 250)} //trim to 130 characters
+                      imageUrl={art.cover}
+                      btn={{
+                        text: "Sign Up",
+                        href: art.link,
+                        type: "primary",
+                        width: "cardButton",
+                        target: "_blank",
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+    
+              <div
+                className={poppinsMedium.className}
+                style={{
+                  marginTop: "20px",
+                }}
+              >
+                <Button
+                  text="Learn More"
+                  href="/articles"
+                  type="primary"
+                  width="largeButton"
+                ></Button>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+  
+}
+
+
+// const Home = ({ events, articles }: HomePageProps) => {
+
+
+//   return (
+//     <>
+//       {/* <Image className={headerImage} src={LandingImage} alt="Landing Image" /> */}
+
+//       <div className={layoutContainer}>
+//         <div className={container}>
+//           <h1 className={title}>WELCOME</h1>
+//           <p style={{ marginRight: "200px" }}>
+//             Taitamariki Potentia is a student-led tech community created to
+//             bridge the gap between university and employment. Our mission is to
+//             empower young people studying technical disciplines to navigate the
+//             challenges associated with breaking into the industry.
+//           </p>
+
+//           <div className={icon}>
+//             <h3 className={h3style}>OUR HISTORY</h3>
+//             {/* <Image
+//               src={Arrow}
+//               alt="Arrow"
+//               className={icon}
+//               width={70}
+//               height={30}
+//             ></Image> */}
+//           </div>
+
+//           <div style={{ display: "flex", justifyContent: "center" }}>
+//             {/* <Image src={BigImage} alt="Town" width={930} height={750} /> */}
+//           </div>
+//         </div>
+
+//         <h3 className={h3style}>Upcoming</h3>
+//         <h1 className={title}>EVENTS</h1>
+
+//         <div className={cardrow}>
+//           {events.slice(0, 3).map((event, index) => (
+//             <div key={index} className={index === 1 ? cardSpace : undefined}>
+//               <Card
+//                 title={event.name}
+//                 body={trimDescription(event.desc, 250)} //trim to 130 characters
+//                 imageUrl={event.cover}
+//                 btn={{
+//                   text: "Sign Up",
+//                   href: event.link,
+//                   type: "primary",
+//                   width: "cardButton",
+//                   target: "_blank",
+//                 }}
+//               />
+//             </div>
+//           ))}
+//         </div>
+
+//         <div
+//           className={poppinsMedium.className}
+//           style={{
+//             marginTop: "20px",
+//           }}
+//         >
+//           <Button
+//             text="Learn More"
+//             href="/events"
+//             type="primary"
+//             width="largeButton"
+//           ></Button>
+//         </div>
+
+//         <div className={MouseImage}>
+//           {/* <Image src={Mouse} alt="Mouse Image" width={700}></Image> */}
+//         </div>
+
+//         <div className={cardContainer}>
+//           <h3 className={h3style}>Latest</h3>
+//           <h1 className={title}>ARTICLES</h1>
+
+//           <div className={cardrow}>
+//             {articles.slice(0, 3).map((art, index) => (
+//               <div key={index} className={index === 1 ? cardSpace : undefined}>
+//                 <Card
+//                   title={art.name}
+//                   body={trimDescription(art.desc, 250)} //trim to 130 characters
+//                   imageUrl={art.cover}
+//                   btn={{
+//                     text: "Sign Up",
+//                     href: art.link,
+//                     type: "primary",
+//                     width: "cardButton",
+//                     target: "_blank",
+//                   }}
+//                 />
+//               </div>
+//             ))}
+//           </div>
+
+//           <div
+//             className={poppinsMedium.className}
+//             style={{
+//               marginTop: "20px",
+//             }}
+//           >
+//             <Button
+//               text="Learn More"
+//               href="/articles"
+//               type="primary"
+//               width="largeButton"
+//             ></Button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+// export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
+//   const articles = await fetchArticlesFromNotion();
+//   const events = await fetchEventsFromNotion();
+//   return { props: { articles, events } };
+// };
