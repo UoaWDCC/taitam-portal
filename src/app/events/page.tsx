c95bd928cb2c1eb58e06471f7fa9f59f2b4e1b42
jrutor <jrut644@aucklanduni.ac.nz>
2023-09-22 15:40:45 +1200

    feat update events to match articles responsiveness

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Poppins } from "next/font/google";
 import { EventCard } from "../(components)/bigCard";
 import Button from "../(components)/Button";
@@ -5,97 +6,204 @@ import stockImg1 from "../(images)/events1.png";
 import stockImg2 from "../(images)/events2.png";
 import stockImg3 from "../(images)/events3.png";
 import React from "react";
+import "../globals.scss";
+import { css } from "@linaria/core";
+import SmallCard from "../(components)/Card";
+import { useState, useEffect } from "react";
 
-const poppinsBlack = Poppins({ weight: "900", subsets: ["latin"] });
+const title = css`
+  padding: 0;
+  color: #f96e47;
+  text-align: left;
+  font-size: 96px;
+
+  @media only screen and (max-width: 600px) {
+    font-size: 36px;
+    padding: 0;
+    color: #f96e47;
+    text-align: left;
+    margin-top: 50px;
+  }
+`;
+
+const root = css`
+  max-width: 1200px;
+  margin: 50px auto 100px auto;
+
+  @media only screen and (max-width: 600px) {
+    margin: 0 20px 0 20px;
+  }
+
+  @media only screen and (max-width: 1275px) {
+    margin: 0 33px 0 33px;
+  }
+`;
+
+const paragraph = css`
+  margin-top: -70px;
+
+  @media only screen and (max-width: 600px) {
+    margin-top: 0px;
+  }
+`;
+
+const smallCard = css`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: nowrap;
+  margin: 0 33px 40px 30px;
+`;
+
 const poppinsMedium = Poppins({ weight: "500", subsets: ["latin"] });
 const poppinsRegular = Poppins({ weight: "400", subsets: ["latin"] });
-const poppinsLight = Poppins({ weight: "300", subsets: ["latin"] });
 
 export default function EventsPage() {
+  const [windowWidth, setWindowWidth] = useState(1920);
+
+  useEffect(() => {
+    // Function to update window width
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Attach event listener for window resize
+    window.addEventListener("resize", updateWindowWidth);
+
+    // Cleanup: remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []); // Empty dependency array means this effect runs only once after initial render
+  const cards =
+    windowWidth < 600 ? (
+      <>
+        <div className={smallCard}>
+          <SmallCard
+            title="Launch Your Tech Career at Our Career Fair"
+            imageUrl="https://t3.ftcdn.net/jpg/02/92/34/14/240_F_292341430_qGtNmxVKgAx4OieUQYNwpMlIbc0ABgVs.jpg"
+            body="Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."
+            btn={{
+              text: "Read More",
+              href: "/articles/first-article",
+              type: "primary",
+              width: "cardButton",
+            }}
+          />
+        </div>
+        <div className={smallCard}>
+          <SmallCard
+            title="Solve Real-World Tech Problems and Win Prizes"
+            imageUrl="https://t4.ftcdn.net/jpg/04/30/92/33/240_F_430923373_9qr0KsEw2uXIeDOJHT8cyOEgTnacm5rl.jpg"
+            body="Work in teams to solve real-world tech problems and present your solutions to a panel of judges for a chance to win prizes. Put your skills to the test and make valuable connections at our annual Hackathon."
+            btn={{
+              text: "Read More",
+              href: "/articles/second-article",
+              type: "secondary",
+              width: "cardButton",
+            }}
+          />
+        </div>
+        <div className={smallCard}>
+          <SmallCard
+            title="Build Your Soft Skills and Stand Out to Employers"
+            imageUrl="https://t4.ftcdn.net/jpg/03/14/34/87/240_F_314348719_6CxqaGP9rfDJwnB1RjntD6V7C6K0Ou6K.jpg"
+            body="Develop your communication, teamwork, and adaptability skills at our Soft Skills Workshop. Led by experienced professionals, this workshop will teach you the skills you need to succeed in the tech industry."
+            btn={{
+              text: "Read More",
+              href: "/articles/third-article",
+              type: "tertiary",
+              width: "cardButton",
+            }}
+          />
+        </div>
+      </>
+    ) : (
+      <>
+        <EventCard
+          title={"Launch Your Tech Career at Our Career Fair"}
+          date={"By Naren Rohan, Project Manager | 12 July 2023"}
+          paragraph={
+            "Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."
+          }
+          image={stockImg1}
+          btn={{
+            text: "Read More",
+            href: "/articles/first-article",
+            type: "primary",
+            width: "cardButton",
+          }}
+        />
+        <EventCard
+          title={"Solve Real-World Tech Problems and Win Prizes"}
+          date={"By Janna Rutor, Director of Events | 5 June 2023"}
+          paragraph={
+            "Work in teams to solve real-world tech problems and present your solutions to a panel of judges for a chance to win prizes. Put your skills to the test and make valuable connections at our annual Hackathon."
+          }
+          image={stockImg2}
+          btn={{
+            text: "Read More",
+            href: "/articles/second-article",
+            type: "secondary",
+            width: "cardButton",
+          }}
+        />
+        <EventCard
+          title={"Build Your Soft Skills and Stand Out to Employers"}
+          date={"By Wesley Key, Hiring Manager | 2 May 2023"}
+          paragraph={
+            "Develop your communication, teamwork, and adaptability skills at our Soft Skills Workshop. Led by experienced professionals, this workshop will teach you the skills you need to succeed in the tech industry."
+          }
+          image={stockImg3}
+          btn={{
+            text: "Read More",
+            href: "/articles/third-article",
+            type: "tertiary",
+            width: "cardButton",
+          }}
+        />
+      </>
+    );
+
   return (
-    <div
-      className={poppinsLight.className}
-      style={{ padding: "100px 160px", backgroundColor: "#FFFDF6" }}
-    >
-      <div
-        className={poppinsBlack.className}
-        style={{ fontSize: 96 + "pt", color: "#F96E47" }}
-      >
-        OUR EVENTS
+    <>
+      <div className={root}>
+        <h1 className={title}>OUR EVENTS</h1>
+        <p className={paragraph}>
+          We regularly host meetups and events in Auckland, providing a vibrant
+          and diverse community for students of all backgrounds who are studying
+          technical disciplines. Our events offer direct access to resources,
+          connections, and advice that can help you take your next career step.
+          Whether you are looking to connect with experienced tech
+          professionals, employers, or recruiters, or simply want to meet other
+          students facing similar challenges, we have something for everyone.
+        </p>
+
+        {cards}
+
+        <div
+          className={poppinsRegular.className}
+          style={{ textAlign: "center", fontSize: "24px", marginTop: "50px" }}
+        >
+          Do you have any questions? Contact us!
+        </div>
+        <div
+          className={poppinsMedium.className}
+          style={{
+            marginTop: "14px",
+            marginBottom: "30px",
+            textAlign: "center",
+          }}
+        >
+          <Button
+            text="Contact Us"
+            href="/contact"
+            type="primary"
+            width="largeButton"
+          ></Button>
+        </div>
       </div>
-      <div style={{ fontSize: 16 + "pt", marginBottom: "25px" }}>
-        We regularly host meetups and events in Auckland, providing a vibrant
-        and diverse community for students of all backgrounds who are studying
-        technical disciplines. Our events offer direct access to resources,
-        connections, and advice that can help you take your next career step.
-        Whether you are looking to connect with experienced tech professionals,
-        employers, or recruiters, or simply want to meet other students facing
-        similar challenges, we have something for everyone.
-      </div>
-      <EventCard
-        title={"Launch Your Tech Career at Our Career Fair"}
-        date={"30 APR 6pm @ OGGB Building, UOA"}
-        paragraph={
-          "Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."
-        }
-        image={stockImg1}
-        btn={{
-          text: "Sign Up",
-          href: "https://events.humanitix.com/test-event-1-o7k7hc2f",
-          type: "primary",
-          width: "cardButton",
-          target: "_blank",
-        }}
-      />
-      <EventCard
-        title={"Launch Your Tech Career at Our Career Fair"}
-        date={"30 APR 6pm @ OGGB Building, UOA"}
-        paragraph={
-          "Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."
-        }
-        image={stockImg2}
-        btn={{
-          text: "Sign Up",
-          href: "https://events.humanitix.com/test-event-2-2d9curo3",
-          type: "primary",
-          width: "cardButton",
-          target: "_blank",
-        }}
-      />
-      <EventCard
-        title={"Launch Your Tech Career at Our Career Fair"}
-        date={"30 APR 6pm @ OGGB Building, UOA"}
-        paragraph={
-          "Connect with top employers, recruiters, and other talented students at our annual Tech Career Fair. Explore job opportunities, receive professional CV advice, and network with industry leaders."
-        }
-        image={stockImg3}
-        btn={{
-          text: "Sign Up",
-          href: "https://events.humanitix.com/test-event-3-hbr5ydfn",
-          type: "primary",
-          width: "cardButton",
-          target: "_blank",
-        }}
-      />
-      <div
-        className={poppinsRegular.className}
-        style={{ textAlign: "center", fontSize: "24pt", marginTop: "50px" }}
-      >
-        Do you have any questions about our events? Contact us!
-      </div>
-      <div
-        className={poppinsMedium.className}
-        style={{
-          marginTop: "20px",
-        }}
-      >
-        <Button
-          text="Contact Us"
-          href="/contact"
-          type="primary"
-          width="largeButton"
-        ></Button>
-      </div>
-    </div>
+    </>
   );
 }
