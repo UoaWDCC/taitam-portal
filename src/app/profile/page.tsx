03671b28361a6e7183c16dc73dba73bec6842afe
amy <yyeo523@aucklanduni.ac.nz>
2023-10-06 16:26:37 +1300

    deployed come css

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,8 +6,6 @@ import { ToastContainer, toast } from "react-toastify";
 import { signWithEmailAndPassword } from "../firebase/firebase";
 
 type Inputs = {
-  name: string;
-  email: string;
   password: string;
   oldPassword: string;
   newPassword: string;
@@ -18,11 +16,11 @@ export default function ProfilePage() {
     <div className = "main">
         <div className="sideBar">
             <div className="profileIcon">
-                <img src="Icon.png" alt="" />
+                <img src="profileIcon.png" alt="" />
             </div>
             <div className="menu">
                 <ul>
-                    <li>Profile</li>
+                    <li>Personal Details</li>
                     <li>Change your password</li>
                     <li>History</li>
                     <li>Upload your CV</li>
@@ -34,16 +32,18 @@ export default function ProfilePage() {
         </div>
         <div className="mainContext">
             <div className="personalDetails">
-                <h2>Personal Details</h2>
-                <li>Fullname: </li>
-                <li>Email: </li>
-                <li>Employment Details: </li>
-                <li>Open for work: </li>
-                <li>Degree of study: </li>
-                <li>Education Level: </li>
+                <h2 className = "header">Personal Details</h2>
+                <ul>
+                    <li>Fullname: </li>
+                    <li>Email: </li>
+                    <li>Employment Details: </li>
+                    <li>Open for work: </li>
+                    <li>Degree of study: </li>
+                    <li>Education Level: </li>
+                </ul>
             </div>
             <div className="changePassword">
-                <h2>Change your password</h2>
+                <h2 className = "header">Change your password</h2>
                 <form action="">
                     <label htmlFor="oldPassword">Old password</label>
                     <input type="password" name="oldPassword" id="oldPassword" />
@@ -54,11 +54,12 @@ export default function ProfilePage() {
                     <button>Submit</button>
                 </form>
             </div>
-            <div className = "Recently viewed Articles">
-
+            <div className = "articlesHistory">
+                <h2 className = "header">Recently viewed articles</h2>
             </div>
-            <div className = "Upload your CV">
-                <button>Upload</button>
+            <div className = "uploadCV">
+                <h2 className = "header">Upload your CV</h2>
+                <button className = "upload">Upload</button>
             </div>
         </div>
     </div>
